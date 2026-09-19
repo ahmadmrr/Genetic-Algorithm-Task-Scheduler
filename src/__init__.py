3832fc48171a9f48e2261655db4cd0a6af5d8bdc
Ahmad Radwan <ahmadmrradwan0@gmail.com>
2026-09-19 11:07:29 +0400

    Test: add unit tests for core GA components

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,11 @@ from src.data_loader import load_data
 from src.genetic_algorithm import genetic_algorithm
 from src.preprocessing import preprocess_employees, preprocess_tasks
 from src.report import generate_report
+
+__all__ = [  # noqa: PLE0604
+    genetic_algorithm,
+    load_data,
+    preprocess_employees,
+    preprocess_tasks,
+    generate_report,
+]
